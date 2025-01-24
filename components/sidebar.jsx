435dbc1ef5ded422140db12245c85912ae920a77
mjgwon24 <alswlchlrh8@naver.com>
2025-01-24 16:12:52 +0900

    fix: url에 따른 sidebar active 기능 추가

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,21 +1,36 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
-  const components = ["Button", "Float Button", "Switch", "Divider", "Checkbox"];
+    const components = ["Button", "Float Button", "Switch", "Divider", "Checkbox"];
+    const pathName = usePathname();
 
-  return (
-    <aside className="border-r p-6 w-52 min-w-52">
-      <ul className="space-y-4">
-        {components.map((component, index) => (
-          <li key={index}>
-            <Link href={`/${component.replace(/\s+/g, "").toLowerCase()}`}>
-              <button className="text-left block w-full text-xl text-gray-700 hover:text-blue-500 transition-colors">
-                {component}
-              </button>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </aside>
-  );
+    return (
+        <aside className="border-r p-6 w-52 min-w-52">
+            <ul className="space-y-4">
+                {components.map((component, index) => {
+                    const componentPath = `/${component.replace(/\s+/g, "").toLowerCase()}`;
+                    const isActive = pathName === componentPath;
+
+                    return (
+                        <li key={index}>
+                            <Link href={componentPath}>
+                                <button
+                                    className={`text-left block w-full transition-colors ${
+                                        isActive
+                                            ? "text-blue-500 font-semibold"
+                                            : "text-gray-700 hover:text-blue-500"
+                                    }`}
+                                >
+                                    {component}
+                                </button>
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </aside>
+    );
 }
