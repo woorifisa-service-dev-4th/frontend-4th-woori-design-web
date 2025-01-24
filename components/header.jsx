760aa0b39fbdb3d53968027c711c1b9b5343a11b
amy8883 <amy8883@naver.com>
2025-01-24 15:34:11 +0900

    feat: 헤더 작업

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,32 @@
 export default function Header() {
-    return (
-        <header className="border">
-            <h1>Header</h1>
-        </header>
-    );
+  return (
+    <header
+      style={{
+        width: "100%",
+        height: "69px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0 24px",
+        borderBottom: "1px solid #E5E5E5",
+        backgroundColor: "white",
+      }}
+    >
+      <img
+        src="/logo.svg"
+        alt="WOORI Design"
+        style={{
+          height: "18px",
+        }}
+      />
+
+      <img
+        src="/member.svg"
+        alt="Team Members"
+        style={{
+          height: "12px",
+        }}
+      />
+    </header>
+  );
 }
