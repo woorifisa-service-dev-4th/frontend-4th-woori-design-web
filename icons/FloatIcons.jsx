69b1d6ee9c731780bb952876f66a1a613e8c6317
mjgwon24 <alswlchlrh8@naver.com>
2025-01-26 22:08:51 +0900

    feat: FloatIcons 추가

---
+++ icons/FloatIcons.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+// 기본 아이콘 (info 아이콘)
+export const InfoIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    width="24px"
+    height="24px"
+  >
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+  </svg>
+);
+
+// 사용자 정의 아이콘 (예: 경고 아이콘)
+export const WarningIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    width="24px"
+    height="24px"
+  >
+    <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" />
+  </svg>
+);
